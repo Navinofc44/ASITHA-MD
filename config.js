ac63382168762adebb77d037fd384b3487fed912
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-27 23:16:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "zaImzCLB#tQOiybZmuS2eZDDgRjj--aQZDHRJa89wivCoM1EL__8",
+MONGODB: process.env.MONGODB || "mongodb://mongo:tZqOskNPFBvoJwXqFTbicqRdmVMzqKtt@autorack.proxy.rlwy.net:59140",//enter mongo db url
 };
